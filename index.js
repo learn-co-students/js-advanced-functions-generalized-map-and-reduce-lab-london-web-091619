f0a1afe54ecdb5fc28eec9d4845091a7371f860e
idmyn <hello@davidmyno.rs>
2019-11-07 14:23:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(arr, func) {
+  const result = []
+  for (let item of arr) {
+    result.push(func(item))
+  }
+  return result
+}
+
+function reduce(arr, func, startingPoint) {
+  let memo = startingPoint ? startingPoint : arr[0]
+  let i = startingPoint ? 0 : 1
+  for (; i < arr.length; i++) {
+    memo = func(arr[i], memo)
+  }
+  return memo
+}
